5fb8d75193d0ddaec3700374a8e1e6a254c20168
xeho91 <xeho91@pm.me>
2021-06-15 15:26:37 +0800

    build(CLI): 🚧  Added `utils`

---
+++ source/cli/utils/log.ts
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+export const error = chalk.red;
+export const info = chalk.blue;
+export const success = chalk.green;
+export const warning = chalk.yellow;
---
+++ source/cli/utils/variants.ts
@@ -0,0 +1,14 @@
+export type VariantGroup = "default"  | "alternative" | "white" | "black";
+
+export const possibleVariants = [
+	"default",
+	"default|gradient",
+	"default|nobg",
+	"alternative",
+	"alternative|gradient",
+	"alternative|nobg",
+	"white",
+	"white|nobg",
+	"black",
+	"black|nobg",
+];
